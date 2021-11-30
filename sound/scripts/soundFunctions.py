7a6d3609af14f9fdb67673c079696ea079786079
SergeyFAKI <91096241+KeysFAKI@users.noreply.github.com>
2021-11-30 10:04:37 +0300

    Update soundFunctions.py

--- sound/scripts/soundFunctions.py
+++ sound/scripts/soundFunctions.py
@@ -1,10 +1,26 @@
 import numpy as np
 
-def speedOfSound(temperature, h2oX, co2Max):
-
-    # Write your function hehe
-
-    co2X = []
-    soundSpeed = []
+def speedOfSound():
     
-    return co2X, soundSpeed
+data = open(r"C:\Users\Keys\Desktop\sound\data\conditions.txt", "r")
+spisoc = data.read().split(" ")
+T = float(spisoc[6]) + 273.15 # температура воздуха
+Vlag = float(spisoc[3]) # влажность воздуха
+R  = 8.314 # постоянная газовая
+
+print("Введите относительный объём углекислого газа в процентах (от 0.03 процента до 5)")
+Procmin = (float(input()) / 100) # относительный объём углексислого газа
+e = 2.7182818284 ** (0.0387 * (T - 273.15)) # нахождение аргумента экспоненциальной функции
+PlVod = e * 1.1742 * Vlag # абсолютная плотность воды
+PlVoz = 1195 # абсолютная плотность воздуха
+PlCO2 = 1700 # абсолютная плотность углекислого газа
+Proc1 = 0.01 # абсолютная влажность
+Procvoz = 1 - Proc1 - Procmin # относительный объём воздуха
+
+μ = (((Procvoz * 28.97) + (Proc1 * 18.01) + (Procmin * 44.01))) # моллекулярная масса
+γ = (((28.97 * 1.0036 * Procvoz) + (18.01 * 1.863 * Proc1) + (44.01 * Procmin)) / ((28.97 * 0.7166 * Procvoz) + (18.01 * 1.403 * Proc1) + (44.01 * 0.649 * Procmin)) * 1000)
+a = float(γ) * float(R) * float(T) / float(μ)
+b = a ** (0.5)
+print("Скорость звука составляет:", b, " м/c")
+    
+return b
