757c46ad10c8f9f857df6ec8b1f050d72234aad8
Andrew <matveichev.ap@phystech.edu>
2021-12-07 14:17:50 +0300

    processing done

--- light/scripts/lightFunctions.py
+++ light/scripts/lightFunctions.py
@@ -5,13 +5,13 @@ from cycler import cycler
 
 def sign(num):
     return -1 if num < 0 else 1
-
+#2560 1440
 def readIntensity(photoName, plotName, data):
     lamp, surface = data
     photo = imageio.imread(photoName)
-    background = photo[480:1030, 1130:1385, 0:3].swapaxes(0, 1)
+    background = photo[510:1330, 870:1370, 0:3].swapaxes(0, 1)
     
-    cut = photo[480:1030, 1170:1350, 0:3].swapaxes(0, 1)
+    cut = photo[510:1330, 870:1370, 0:3].swapaxes(0, 1)
     rgb = np.mean(cut, axis=(0))
     
     luma = 0.2989 * rgb[:, 0] + 0.5866 * rgb[:, 1] + 0.1144 * rgb[:, 2]
@@ -36,11 +36,12 @@ def readIntensity(photoName, plotName, data):
 
 def calibration(luma_mercury_big):
     coef = 5
-    cor = len(luma_mercury_big)//coef + 1
+    cor = len(luma_mercury_big)//coef
     luma_mercury = np.array([0]*cor)
 
-    for i in range(550):
+    for i in range(len(luma_mercury_big)):
         luma_mercury[i//coef] += luma_mercury_big[i]
+
     derivative = luma_mercury[1:] - luma_mercury[:-1]
 
     measured_max = []
@@ -48,8 +49,23 @@ def calibration(luma_mercury_big):
         if (sign(derivative[i]) == sign(-derivative[i-1])) and (derivative[i] != 0 and derivative[i-1] != 0):
             measured_max.append(i)
     measured_max = np.array(measured_max)*coef
+    print (measured_max)
     real_max = [404.6563, 435.8328, 546.0735, 578.2] #данные с википедии
-    k = (real_max[3] - real_max[0])/(measured_max[10] - measured_max[0])
-    return [int(real_max[0] - measured_max[0]*k), int((len(luma_mercury_big) - measured_max[0])*k + real_max[0])]
+    k = (578.2 - 404.6563)/(measured_max[4] - measured_max[0])
+
+    print(measured_max)
+
+    return [int(404.6563 - measured_max[0]*k), int((len(luma_mercury_big) - measured_max[0])*k + 404.6563)]
+
+def data(names): #фуекция обработки названия файла для получения подписей графиков
+    name = names.split("-")
+    if (name[0] == 'blue'): surface = "синий лист"
+    if (name[0] == 'green'): surface = "зеленый лист"
+    if (name[0] == 'red'): surface = "красный лист"
+    if (name[0] == 'white'): surface = "белый лист"
+    if (name[0] == 'yellow'): surface = "желтый лист"
+    if (name[1] == 'tungsten.jpg'): lamp = "лампа накаливания"
+    if (name[1] == 'mercury.jpg'): lamp = "ртутная лампа"
+    return [lamp, surface]
 
 def nice(): print("Nice") #функция которая выводит Nice
--- light/scripts/lightProcessing.py
+++ light/scripts/lightProcessing.py
@@ -2,47 +2,36 @@ import lightFunctions as func
 from matplotlib import pyplot as plt
 import numpy as np
 
-def data(names): #фуекция обработки названия файла для получения подписей графиков
-	name = names.split("-")
-	if (name[0] == 'blue'): surface = "синий лист"
-	if (name[0] == 'green'): surface = "зеленый лист"
-	if (name[0] == 'red'): surface = "красный лист"
-	if (name[0] == 'white'): surface = "белый лист"
-	if (name[0] == 'yellow'): surface = "желтый лист"
-	if (name[1] == 'tungsten.png'): lamp = "лампа накаливания"
-	if (name[1] == 'mercury.png'): lamp = "ртутная лампа"
-	return [lamp, surface]
-
-def graph(lumas, label, x, y, name, y_label):
-	fig, ax = plt.subplots(figsize=(11, 9), dpi=200)
+def graph(lumas, label, x1, x2, name, y_label):
+	fig, ax = plt.subplots(figsize=(11, 9), dpi=60)
 	colors = ['b','g','r','w','y']
-	lengh = np.linspace(x, y, len(lumas[5]))
+	lengh = np.linspace(x1, x2, len(lumas[5]))
 	for i in range(5):
-		plt.plot(lengh, lumas[i], alpha=1, label="first", lw=1.9, c=colors[i], mew=0.4, ms=0, mec = 'b')
+		plt.plot(lengh, lumas[i], lw=1.9, c=colors[i], mew=0.4, ms=0, mec = 'b')
 
 	plt.minorticks_on()
 	plt.grid(which='major', linewidth = 0.5, color = 'k')
 	plt.grid(which='minor', linestyle = '-')
 	ax.set_facecolor((0.85, 0.85, 0.85))
 
-	plt.axis([x - 5,y + 5,0,max(lumas[3])*1.05])
+	plt.axis([x1 - 5,x2 - 55,0,max(lumas[3])*1.05])
 	plt.legend(["синий лист", "зеленый лист", "красный лист", "белый лист", "желтый лист"])
 	plt.title(label, fontsize = 13)   
 	plt.ylabel(y_label, fontsize = 13)   
 	plt.xlabel('Длинна волны [нм]', fontsize = 13)   
 	plt.savefig(name)
 
-pics = ["blue-tungsten.png", "green-tungsten.png", "red-tungsten.png","white-tungsten.png", "yellow-tungsten.png", "white-mercury.png"]
+pics = ["blue-tungsten.jpg", "green-tungsten.jpg", "red-tungsten.jpg","white-tungsten.jpg", "yellow-tungsten.jpg", "white-mercury.jpg"]
 
 print("Считыываем данные и строим графики интенсивности отраженного излучения")
 lumas = []
 for name in pics:
-	luma = func.readIntensity(name, name.split(".")[0]+"-plot.jpeg", data(name))
+	luma = func.readIntensity(name, name.split(".")[0]+"-plot.jpeg", func.data(name))
 	lumas.append(luma)
 lumas = np.array(lumas)
-x, y = func.calibration(lumas[5])
-lumas[3] += 1
+x1, x2 = func.calibration(lumas[5])
+lumas[3] += 0.7
 
 # строим график зависимости
-graph(lumas, "Отраженная интенсивность\n излучения лампы накаливания", x, y, "intensites.png", 'Яркость')
-graph(lumas/lumas[3], "Альбедо поверхностей", x, y, "albedos.png", 'Альбедо')
+graph(lumas, "Отраженная интенсивность\n излучения лампы накаливания", x1, x2, "intensites.png", 'Яркость')
+graph(lumas/lumas[3], "Альбедо поверхностей", x1, x2, "albedos.png", 'Альбедо')
